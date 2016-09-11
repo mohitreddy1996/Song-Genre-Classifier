3706ac160c53b410aba1bb897da33a7f239cf13f
Mohit Reddy <mohitreddy1996@gmail.com>
2016-09-11 14:41:34 +0530

    Inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+                Tentative Document
+    ------------------------------------------
+    A code which will train itself from going through songs on youtube and given genres for them.
+    Next any song given as input, user will be given the output(predicted genre).
+    Planning to use Classification algorithms for predicting and training data. Also supervised and unsupervised learning algorithms.
+    Need to find a module or an api for fetching songs from youtube at a fixed frequency or same quality.
+
+"""
