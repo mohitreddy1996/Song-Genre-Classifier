1122f3b960500bce955f7662fde303e7c8866b97
Mohit Reddy <mohitreddy1996@gmail.com>
2016-09-17 12:35:50 +0530

    Project Document added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
                 Tentative Document
     ------------------------------------------
-    A code which will train itself from going through songs on youtube and given genres for them.
+    A code which will train itself from songs in .wav format.
+    Planning to learn Signal/Audio processing using python. Try feature extraction.
     Next any song given as input, user will be given the output(predicted genre).
     Planning to use Classification algorithms for predicting and training data. Also supervised and unsupervised learning algorithms.
     Need to find a module or an api for fetching songs from youtube at a fixed frequency or same quality.
